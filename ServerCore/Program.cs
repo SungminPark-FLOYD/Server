8b5470bff64144f99905ddeaaaa2e7f78d56d5b2
FLOYD <sungmin.park5941@gmail.com>
2023-08-08 15:01:20 +0900

    #Lock 기초

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ServerCore
+﻿using System.Threading;
+
+namespace ServerCore
 {
     class Program
     {
@@ -134,6 +136,54 @@
         //    for (int i = 0; i < 100000; i++)
         //    {
         //        Interlocked.Decrement(ref number);
+        //    }
+
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    Task t1 = new Task(Thread_1);
+        //    Task t2 = new Task(Thread_2);
+        //    t1.Start();
+        //    t2.Start();
+
+        //    Task.WaitAll(t1, t2);
+
+        //    Console.WriteLine(number);
+        //}
+        #endregion
+
+        #region Lock
+        //static int number = 0;
+        //static object _obj = new object();
+
+        //static void Thread_1()
+        //{
+
+        //    for (int i = 0; i < 100000; i++)
+        //    {
+        //        //상호배제 Mutual Exclusive
+        //        //1) 싱글쓰레드 처럼 사용할 수 있음
+        //        //2)Exit를 해주지 않으면 DeadLock상황이 나타날 수 있음
+        //        //3)해결방법 -> try - catch 문 활용
+
+        //        //Monitor.Enter(_obj); //문을 잠구는 행위
+        //        //number++;
+        //        //Monitor.Exit(_obj); //잠금을 풀어준다
+
+        //        lock(_obj)
+        //        {
+        //            number++;
+        //        }
+        //    }
+
+        //}
+
+        //static void Thread_2()
+        //{
+        //    lock (_obj)
+        //    {
+        //        number--;
         //    }
 
         //}
