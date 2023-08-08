4ea02425562b7536756ecbb910a27775fa71a12d
FLOYD <sungmin.park5941@gmail.com>
2023-08-08 17:14:21 +0900

    # SpinLock / Context Switching

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,8 +2,7 @@
 
 namespace ServerCore
 {
-    class Program
-    {
+    
         #region 컴파일러 최적화
         ////volatile 휘발성 메모리 => 권장하지 않음   
         //volatile static bool _stop = false;
@@ -200,5 +199,90 @@ namespace ServerCore
         //    Console.WriteLine(number);
         //}
         #endregion
+
+        #region SpinLock / Context Switching
+        //대기하고있는 쓰레드가 잠금이 풀린후 동시에 작업을 하는 경우가 생김
+        //방에 들어가서 문을 잠그는 것을 2동작으로 나누지 말고 한번에 처리
+        //class SpinLock
+        //{
+        //    volatile int _locked = 0;
+
+        //    public void Acquire()
+        //    {
+        //        while(true)
+        //        {
+        //            ////잠김이 풀리기를 기다린다
+        //            //int original = Interlocked.Exchange(ref _locked, 1);
+        //            //if (original == 0)
+        //            //    break;
+
+        //            //CAS Copare_And_Swap
+        //            //int original = Interlocked.CompareExchange(ref _locked, 1, 0);
+        //            //if(original == 0)
+        //            //    break;
+
+        //            //가독성 증가
+        //            int expected = 0;
+        //            int desired = 1;
+        //            if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
+        //                break;
+
+        //            //쉬기
+        //            Thread.Sleep(1); //무조건 휴식 => 무조건 1ms 정도 쉬기
+        //            Thread.Sleep(0); //조건부 양보 => 나보다 우선순위가 낮은 애들한테는 양보 불가
+        //            Thread.Yield(); //관대한 양보 => 실행가능한 쓰레드먼저 양보 => 남은 시간은 본인에게 소진
+
+        //        }
+        //    }
+
+        //    public void Relese()
+        //    {
+        //        _locked = 0;
+        //    }
+        //}
+        //class Program
+        //{
+        //    static int _num = 0;
+        //    static SpinLock _lock = new SpinLock();
+
+        //    static void Thread_1()
+        //    {
+        //        for(int i = 0; i < 10000; i++)
+        //        {
+        //            _lock.Acquire();
+        //            _num++;
+        //            _lock.Relese();
+        //        }
+        //    }
+        //    static void Thread_2()
+        //    {
+        //        for (int i = 0; i < 10000; i++)
+        //        {
+        //            _lock.Acquire();
+        //            _num--;
+        //            _lock.Relese();
+        //        }
+        //    }
+        //    static void Main(string[] args)
+        //    {
+        //        Task t1 = new Task(Thread_1);
+        //        Task t2 = new Task(Thread_2);
+        //        t1.Start();
+        //        t2.Start();
+
+        //        Task.WaitAll(t1, t2);
+
+        //        Console.WriteLine(_num);
+
+        //    }
+        //}
+        #endregion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
     }
+        
 }
