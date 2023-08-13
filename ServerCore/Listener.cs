f45685079420d30e27e7c37b19a6516eb208742b
FLOYD <sungmin.park5941@gmail.com>
2023-08-13 15:51:25 +0900

    #Listener(비동기 방식)

---
+++ ServerCore/Listener.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Listener
+    {
+        Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
+
+        //문지기 생성
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler += onAcceptHandler;
+
+            //문지기 교육
+            _listenSocket.Bind(endPoint);
+
+            //영업 시작
+            //backlog : 최대 대기수
+            _listenSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            //OnAcceptCompleted도 형식에 맞춰서 작성해줘야 한다
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            //최초 등록
+            RegisterAccept(args);
+        }
+
+        //예약 함수
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            //재사용하기 위해 기존의 정보 초기화
+            args.AcceptSocket = null;
+
+            //AcceptAsync : 비동기 함수
+            bool pending = _listenSocket.AcceptAsync(args);
+            if (pending == false)
+                OnAcceptCompleted(null, args);
+        }
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            //return한 후 들어오는 것 처리
+            if(args.SocketError == SocketError.Success)
+            {
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+            else
+                Console.WriteLine(args.SocketError.ToString());
+            
+            //다음을 위해 다시 등록
+            RegisterAccept(args);
+        }
+
+        ////손님 입장
+        //public Socket Accept()
+        //{          
+        //    //블로킹 계열 함수는 피해야 한다 : 입장요청을 주기 전까지 무한 대기를 하기 때문
+        //    return _listenSocket.Accept();
+        //}
+    }
+}
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -7,55 +7,49 @@ namespace ServerCore
 {
     class Program
     {
+        //문지기
+        static Listener _listener = new Listener();
+
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try
+            {
+                //받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Client] {recvData}");
+
+                //보낸다
+                byte[] senBuff = Encoding.UTF8.GetBytes("Welcom to MMORPG Server!");
+                clientSocket.Send(senBuff);
+
+                //쫒아낸다
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }            
+        }
         static void Main(string[] args)
         {
             //DNS -> 도메인 주소를 통해서 ip주소 찾기
             string host = Dns.GetHostName();
             IPHostEntry ipHost = Dns.GetHostEntry(host);
             IPAddress ipAddr = ipHost.AddressList[0];
-            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
-
-            //문지기
-            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);          
             
-            try
+          
+            _listener.Init(endPoint, OnAcceptHandler);
+            Console.WriteLine("Listening...");
+            while (true)
             {
-                //문지기 교육
-                listenSocket.Bind(endPoint);
-
-                //영업 시작
-                //backlog : 최대 대기수
-                listenSocket.Listen(10);
-
-                while (true)
-                {
-                    Console.WriteLine("Listening...");
-
-                    //손님입장
-                    Socket clientSocket = listenSocket.Accept();
-
-                    //받는다
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = clientSocket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From Client] {recvData}");
-
-                    //받는다
-                    byte[] senBuff = Encoding.UTF8.GetBytes("Welcom to MMORPG Server!");
-                    clientSocket.Send(senBuff);
-
-                    //쫒아낸다
-                    clientSocket.Shutdown(SocketShutdown.Both);
-                    clientSocket.Close();
-                }
+                //손님입장
+                //Socket clientSocket = _listener.Accept();                   
             }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            
-
-            
+                     
         }
     }
     
