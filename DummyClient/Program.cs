9d811b65c95ff7f143e48e7e02821572f252bde4
FLOYD <sungmin.park5941@gmail.com>
2023-08-16 14:05:29 +0900

    #Serialization_1

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -4,53 +4,7 @@ using System.Net.Sockets;
 using System.Text;
 
 namespace DummyClient
-{
-    class Packet
-    {
-        public ushort size;
-        public ushort packetId;
-
-    }
-
-    class GameSession : Session
-    {
-        public override void OnConnected(EndPoint endPoint)
-        {
-            Console.WriteLine($"OnConnected : {endPoint}");
-
-            Packet packet = new Packet() { size = 4, packetId = 7 };
-
-            for (int i = 0; i < 5; i++)
-            {
-                //보낸다
-                ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-                byte[] buffer = BitConverter.GetBytes(packet.size);
-                byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
-                Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
-                Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
-                ArraySegment<byte> sendBuff = SendBufferHelper.Close(packet.size);
-
-                Send(sendBuff);
-            }
-        }
-
-        public override void OnDisconnected(EndPoint endPoint)
-        {
-            Console.WriteLine($"OnDisconnected : {endPoint}");
-        }
-
-        public override int OnRecv(ArraySegment<byte> buffer)
-        {
-            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
-            Console.WriteLine($"[From Server] {recvData}");
-            return buffer.Count;    
-        }
-
-        public override void OnSend(int numOfByte)
-        {
-            Console.WriteLine($"Transferred bytes : {numOfByte}");
-        }
-    }
+{  
 
     class Program
     {
@@ -64,7 +18,7 @@ namespace DummyClient
 
             Connecter connecter = new Connecter();
 
-            connecter.Connect(endPoint, () => { return new GameSession(); });
+            connecter.Connect(endPoint, () => { return new ServerSession(); });
 
             while(true)
             {
---
+++ DummyClient/ServerSession.cs
@@ -0,0 +1,100 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyClient
+{
+    class Packet
+    {
+        public ushort size;
+        public ushort packetId;
+
+    }
+
+    class PlayerInfoReq : Packet
+    {
+        public long playerId;
+    }
+
+    class PlayerInfoOk : Packet
+    {
+        public int hp;
+        public int attack;
+    }
+
+    public enum PacketID
+    {
+        PlayerInfoReq = 1,
+        PlayerInfoOk = 2,   
+    }
+
+    class ServerSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            PlayerInfoReq packet = new PlayerInfoReq() { size = 4, packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001};
+
+            //for (int i = 0; i < 5; i++)
+            {
+                //보낸다
+                ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+                bool success = true;
+                //반드시 ushort버전으로 넣어야 2바이트가 입력된다
+                ushort count = 0;
+
+                //success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), packet.size);
+                count += 2;
+                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.packetId);
+                count += 2;
+                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.playerId);
+                count += 8;
+
+                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+                //이전버전
+                {
+                    //byte[] size = BitConverter.GetBytes(packet.size);
+                    //byte[] packetId = BitConverter.GetBytes(packet.packetId);
+                    //byte[] playerId = BitConverter.GetBytes(packet.playerId);
+
+                    ////데이터 크기만큼 카운트 증가       
+                    //Array.Copy(size, 0, s.Array, s.Offset + count, 2);
+                    //count += 2;
+                    //Array.Copy(packetId, 0, s.Array, s.Offset + count, 2);
+                    //count += 2;
+                    //Array.Copy(playerId, 0, s.Array, s.Offset + count, 8);
+                    //count += 8;
+                }
+
+                ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
+
+                if(success)
+                    Send(sendBuff);
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override int OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Server] {recvData}");
+            return buffer.Count;
+        }
+
+        public override void OnSend(int numOfByte)
+        {
+            Console.WriteLine($"Transferred bytes : {numOfByte}");
+        }
+    }
+}
